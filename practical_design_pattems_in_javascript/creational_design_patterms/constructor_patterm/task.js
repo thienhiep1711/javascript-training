2752176f08ce227cbbb2e3e7de59e68aaefbd055
Hiep Nguyen <thienhiep1711@users.noreply.github.com>
2018-09-11 17:45:28 +0700

    complete constructor patterm

---
+++ practical_design_pattems_in_javascript/creational_design_patterms/constructor_patterm/task.js
@@ -0,0 +1,22 @@
+const Task = function(name) {
+    this.name = name;
+    this.completed = false;
+    this.complete = function() {
+        console.log('Completing task:' + this.name)
+        this.completed = true;
+    }
+
+    this.save = function() {
+        console.log('saving task:' + this.name);
+    }
+}
+
+const task1 = new Task('create a demo for constructors');
+const task2 = new Task('create a demo for modules');
+const task3 = new Task('create a demo for singletons');
+const task4 = new Task('create a demo for prototypes');
+
+task1.complete();
+task2.save();
+task3.save();
+task4.save();
